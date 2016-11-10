cdb2e76396d47f29dd4b36ddb5f2c67c24318416
Amila Weerasinghe <amila@wustl.edu>
2016-11-10 14:33:01 -0600

    Density clustering probability automated

---
+++ DingLab/DensityWithError/MembershipProbability.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+d <- read.table(args[1], header=TRUE)
+
+library(ggplot2)
+
+d$Variant <- factor(d$Variant, levels = d$Variant)
+
+sp <- ggplot(d, aes(x=Variant,y=ClusterID)) + geom_point(aes(colour = Probability), size=3) + scale_colour_gradient(low = "red", high = "blue")
+
+#sp <- ggplot(d, aes(x=Variant,y=ClusterID)) + geom_point(aes(size=Probability))
+
+sp+theme_bw() + theme(axis.text.x=element_text(angle=90, size=6)) + ggtitle("Cluster Membership Probabilities")
+ggsave("./Results/ProbabilityPlot.pdf", width = 23.6, height = 13.3)
